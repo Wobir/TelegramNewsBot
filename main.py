0e575536c97407c9d6b88cba8c52b60000db58fb
wobir <ilyakorobi4un@yandex.ru>
2025-08-02 17:03:37 +0500

    Main update

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ with open("config.yaml", "r", encoding="utf-8") as f:
 
 API_TOKEN = config["api_token"]
 OWNER_ID = config["owner_id"]
-CHANNEL_ID = int(config["channel_id"])  # убедись, что int!
+CHANNEL_ID = config["channel_id"]  # убедись, что int!
 
 bot = Bot(
     token=API_TOKEN,
